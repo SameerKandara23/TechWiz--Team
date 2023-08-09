bb47bc33ef1a89045a566f43630254ec5f392d62
hawa <evayasir4@gmail.com>
2023-08-09 17:26:47 +0500

    firt commit

---
+++ resources/views/layouts/adminDashboard_layout.blade.php
@@ -0,0 +1,176 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Spica Admin</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="project/dashboard/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="project/dashboard/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="project/dashboard/css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="project/dashboard/images/favicon.png" />
+<style>
+  
+</style>
+</head>
+<body>
+  <div class="container-scroller d-flex">
+    <!-- partial:./partials/_sidebar.html -->
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+      <ul class="nav">
+
+        <li class="nav-item">
+          <a class="nav-link" href="AdminDashboard.html">
+            <img src="project/dashboard/images/icons/1.png" width="20px" /> &nbsp;
+            &nbsp;
+            <span class="menu-title">Admin Dashboard</span>
+            <div class="badge badge-info badge-pill">2</div>
+          </a>
+        </li>
+        
+        <li class="nav-item" style="background-color:black;">
+          <a class="nav-link" href="project/dashboard/html/activefetch.html">
+            
+            <img src="project/dashboard/images/icons/2.png" width="20px" /> &nbsp;
+            &nbsp;
+            <span class="menu-title">Active User</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/contactfetch.html">
+            <img src="project/dashboard/images/icons/5.png" width="20px" /> &nbsp;
+            &nbsp; <span class="menu-title">Contacts Fetch</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/feedbackfetch.html">
+           
+            <img src="project/dashboard/images/icons/3.png" width="20px" /> &nbsp;
+            &nbsp;   <span class="menu-title">Feedback Fetch</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/orderfetch.html">
+           
+            <img src="project/dashboard/images/icons/images__1_-removebg-preview.png" width="20px" /> &nbsp;
+            &nbsp; <span class="menu-title">Order Fetch</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/addproduct.html">
+            <img src="project/dashboard/images/icons/download__3_-removebg-preview.png" width="20px" /> &nbsp;
+            &nbsp;      <span class="menu-title">Add Product</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/fetchproduct.html">
+            <img src="project/dashboard/images/icons/10.png" width="20px" /> &nbsp;
+            &nbsp;     <span class="menu-title">Fetch Product</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/addpopularproduct.html">
+            <img src="project/dashboard/images/icons/9.png" width="20px" /> &nbsp;
+            &nbsp;     <span class="menu-title">Add Popular Product</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/fetchpopular.html">
+            <img src="project/dashboard/images/icons/download-removebg-preview-(1).png" width="20px" /> &nbsp;
+            &nbsp;     <span class="menu-title">Fetch Popular Product</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/addteam.html">
+            <img src="project/dashboard/images/icons/addt.png" width="20px" /> &nbsp;
+            &nbsp;    <span class="menu-title">Add Team</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/fetchteam.html">
+            <img src="project/dashboard/images/icons/fetcht.png" width="20px" /> &nbsp;
+            &nbsp;   <span class="menu-title">Fetch Team</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/addvideo.html">
+            <img src="project/dashboard/images/icons/7.png" width="20px" /> &nbsp;
+            &nbsp;     <span class="menu-title">Add Video </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/fetchvedio.html">
+            <img src="project/dashboard/images/icons/8.png" width="20px" /> &nbsp;
+            &nbsp;       <span class="menu-title">Fetch Video</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/addgallery.html">
+            <img src="project/dashboard/images/icons/addt.png" width="20px" /> &nbsp;
+            &nbsp;       <span class="menu-title">Add  Gallery</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project/dashboard/html/fetchgallery.html">
+            <img src="project/dashboard/images/icons/download__2_-removebg-preview.png" width="20px" /> &nbsp;
+            &nbsp;      <span class="menu-title">Fetch  Gallery</span>
+          </a>
+        </li>
+        
+      </ul>
+    </nav>
+   
+
+
+
+@yield("page_content")
+
+
+
+
+        <footer class="footer">
+          <div class="card">
+            <div class="card-body">
+              <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+                <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Distributed By: <a href="https://www.themewagon.com/" target="_blank">ThemeWagon</a></span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap dashboard templates</a> from Bootstrapdash.com</span>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- base:js -->
+  <script src="project/dashboard/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <script src="project/dashboard/vendors/chart.js/Chart.min.js"></script>
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="project/dashboard/js/off-canvas.js"></script>
+  <script src="project/dashboard/js/hoverable-collapse.js"></script>
+  <script src="project/dashboard/js/template.js"></script>
+  <!-- endinject -->
+  <!-- plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- Custom js for this page-->
+  <script src="project/dashboard/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
+</body>
+
+</html>
---
+++ resources/views/layouts/welcomeLayout.blade.php
@@ -0,0 +1,218 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- set the encoding of your site -->
+	<meta charset="utf-8">
+	<!-- set the Compatible of your site -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- set the page title -->
+	<title>Plant Nest</title>
+	<!-- include the site Google Fonts stylesheet -->
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700%7CRoboto:300,400,500,700,900&amp;display=swap" rel="stylesheet">
+	<!-- include the site bootstrap stylesheet -->
+	<link rel="stylesheet" href="project/template/css/bootstrap.css">
+	<!-- include the site fontawesome stylesheet -->
+	<link rel="stylesheet" href="project/template/css/fontawesome.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="project/template/style.css">
+	<!-- include theme plugins setting stylesheet -->
+	<link rel="stylesheet" href="project/template/css/plugins.css">
+	<!-- include theme color setting stylesheet -->
+	<link rel="stylesheet" href="project/template/css/color.css">
+	<!-- include theme responsive setting stylesheet -->
+	<link rel="stylesheet" href="project/template/css/responsive.css">
+</head>
+<body class="antialiased">
+
+
+
+
+
+
+	<!-- pageWrapper -->
+	<div id="pageWrapper">
+		<!-- pageHeader -->
+		<header id="header" class="pt-lg-5 pt-md-3 pt-2 position-absolute w-100">
+			<div class="container-fluid px-xl-17 px-lg-5 px-md-3 px-0 d-flex flex-wrap">
+				<div class="col-6 col-sm-3 col-lg-2 order-sm-2 order-md-0 dis-none">
+					<!-- langList -->
+					<ul class="nav nav-tabs langList pt-xl-6 pt-lg-4 pt-3 border-bottom-0 nav nav-tabs wishList pt-xl-5 pt-lg-4 pt-3 mr-xl-3 mr-0 justify-content-end border-bottom-0">
+                    <li class="nav-item"><a class="nav-link position-relative icon-cart" href="javascript:void(0);"><span class="num rounded d-block">2</span></a></li>
+
+
+						<li class="dropdown">
+							<a class="dropdown-toggle text-uppercase" data-toggle="dropdown" href="javascript:void(0);" role="button" aria-haspopup="true" aria-expanded="false">ENG</a>
+							<div class="dropdown-menu pl-4 pr-4">
+								<a class="dropdown-item" href="javascript:void(0);">English</a>
+								<a class="dropdown-item" href="javascript:void(0);">Vietnamese</a>
+								<a class="dropdown-item" href="javascript:void(0);">French</a>
+							</div>
+						</li>
+						<li class="dropdown">
+							<a class="dropdown-toggle text-uppercase" data-toggle="dropdown" href="javascript:void(0);" role="button" aria-haspopup="true" aria-expanded="false">USD</a>
+							<div class="dropdown-menu text-uppercase pl-4 pr-4 border-0">
+								<a class="dropdown-item" href="javascript:void(0);">USD</a>
+								<a class="dropdown-item" href="javascript:void(0);">VND</a>
+								<a class="dropdown-item" href="javascript:void(0);">euro</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div class="col-12 col-sm-6 col-lg-8 static-block">
+					<!-- mainHolder -->
+					<div class="mainHolder justify-content-center">
+						<!-- pageNav1 -->
+						<nav class="navbar navbar-expand-lg navbar-light p-0 pageNav1 position-static">
+							<button type="button" class="navbar-toggle collapsed position-relative mt-md-2" data-toggle="collapse" data-target="#navbarNav" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarNav">
+								<ul class="navbar-nav mx-auto text-uppercase d-inline-block">
+									<li class="nav-item active dropdown">
+										<a class="dropdown-toggle d-block" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">home</a>
+										<ul class="list-unstyled text-capitalize border-right border-bottom border-left dropdown-menu mt-0 py-0">
+											<li class="d-block mx-0"><a href="home.html">Home 1</a></li>
+											<li class="d-block mx-0"><a href="home2.html">Home 2</a></li>
+											<li class="d-block mx-0"><a href="home3.html">Home 3</a></li>
+										</ul>
+									</li>
+									<li class="nav-item dropdown">
+										<a class="dropdown-toggle d-block" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Store</a>
+										<ul class="list-unstyled text-capitalize border-right border-bottom border-left dropdown-menu mt-0 py-0">
+											<li class="d-block mx-0"><a href="shop.html">Shop Left Sidebar</a></li>
+											<li class="d-block mx-0"><a href="shop-detail.html">Single Product</a></li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a class="d-block" href="about-us.html">About</a>
+									</li>
+									<li class="nav-item">
+										<a class="nLogo" href="home.html"><img src="project/template/images/logo.png" alt="Botanical" class="img-fluid"></a>
+									</li>
+									<li class="nav-item dropdown">
+										<a class="dropdown-toggle d-block" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog</a>
+										<ul class="list-unstyled text-capitalize border-right border-bottom border-left dropdown-menu mt-0 py-0">
+											<li class="d-block mx-0"><a href="blog.html">Blog Left Sidebar</a></li>
+											<li class="d-block mx-0"><a href="blog-detail.html">Blog Detail</a></li>
+										</ul>
+									</li>
+									<li class="nav-item dropdown">
+										<a class="dropdown-toggle d-block" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+										<ul class="list-unstyled text-capitalize border-right border-bottom border-left dropdown-menu mt-0 py-0">
+											<li class="d-block mx-0"><a href="cart-page.html">Cart Page</a></li>
+										</ul>
+									</li>
+									<li class="nav-item">
+										<a class="d-block" href="contact-us.html">contact</a>
+									</li>
+
+
+                                  
+								</ul>
+							</div>
+						</nav>
+						<div class="logo">
+							<a href="#"><img src="project/template/images/logo.png" alt="Botanical" class="img-fluid"></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-6 col-sm-3 col-lg-2 order-sm-3 order-md-0 dis-none">
+					<!-- wishList -->
+					<ul class="nav nav-tabs wishList pt-xl-5 pt-lg-4 pt-3 mr-xl-3 mr-0 justify-content-end border-bottom-0">
+
+                    
+                        
+						<li class="nav-item">    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Signin</a></li>
+                        <li class="nav-item"> <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">login</a>
+</li>
+						
+
+                       
+					</ul>
+				</div>
+			</div>
+		</header>
+		<!-- main -->
+		
+
+
+         @yield("page_content")
+
+
+
+			<!-- footerHolder -->
+			<aside class="footerHolder container-fluid overflow-hidden px-xl-20 px-lg-14 pt-xl-12 pb-xl-8 pt-lg-12 pt-md-8 pt-10 pb-lg-8">
+				<div class="d-flex flex-wrap flex-lg-nowrap">
+					<div class="coll-1 pr-3 mb-sm-4 mb-3 mb-lg-0">
+						<h3 class="headingVI fwEbold text-uppercase mb-7">Contact Us</h3>
+						<ul class="list-unstyled footerContactList mb-3">
+							<li class="mb-3 d-flex flex-nowrap"><span class="icon icon-place mr-3"></span> <address class="fwEbold m-0">Address: London Oxford Street, 012 United Kingdom.</address></li>
+							<li class="mb-3 d-flex flex-nowrap"><span class="icon icon-phone mr-3"></span> <span class="leftAlign">Phone : <a href="javascript:void(0);">(+032) 3456 7890</a></span></li>
+							<li class="email d-flex flex-nowrap"><span class="icon icon-email mr-2"></span> <span class="leftAlign">Email:  <a href="javascript:void(0);">Botanicalstore@gmail.com</a></span></li>
+						</ul>
+						<ul class="list-unstyled followSocailNetwork d-flex flex-nowrap">
+							<li class="fwEbold mr-xl-11 mr-sm-6 mr-4">Follow  us:</li>
+							<li class="mr-xl-6 mr-sm-4 mr-2"><a href="javascript:void(0);" class="fab fa-facebook-f"></a></li>
+							<li class="mr-xl-6 mr-sm-4 mr-2"><a href="javascript:void(0);" class="fab fa-twitter"></a></li>
+							<li class="mr-xl-6 mr-sm-4 mr-2"><a href="javascript:void(0);" class="fab fa-pinterest"></a></li>
+							<li class="mr-2"><a href="javascript:void(0);" class="fab fa-google-plus-g"></a></li>
+						</ul>
+					</div>
+					<div class="coll-2 mb-sm-4 mb-3 mb-lg-0">
+						<h3 class="headingVI fwEbold text-uppercase mb-6">Information</h3>
+						<ul class="list-unstyled footerNavList">
+							<li class="mb-1"><a href="javascript:void(0);">New Products</a></li>
+							<li class="mb-2"><a href="javascript:void(0);">Top Sellers</a></li>
+							<li class="mb-2"><a href="javascript:void(0);">Our Blog</a></li>
+							<li class="mb-2"><a href="javascript:void(0);">About Our Shop</a></li>
+							<li><a href="javascript:void(0);">Privacy policy</a></li>
+						</ul>
+					</div>
+					<div class="coll-3 mb-sm-4 mb-3 mb-lg-0">
+						<h3 class="headingVI fwEbold text-uppercase mb-6">My Account</h3>
+						<ul class="list-unstyled footerNavList">
+							<li class="mb-1"><a href="javascript:void(0);">My account</a></li>
+							<li class="mb-2"><a href="javascript:void(0);">Discount</a></li>
+							<li class="mb-2"><a href="javascript:void(0);">Orders history</a></li>
+							<li><a href="javascript:void(0);">Personal information</a></li>
+						</ul>
+					</div>
+					<div class="coll-4 mb-sm-4 mb-3 mb-lg-0">
+						<h3 class="headingVI fwEbold text-uppercase mb-7 pl-xl-14 pl-lg-10">Popular Tag</h3>
+						<ul class="list-unstyled tagNavList d-flex flex-wrap justify-content-lg-end mb-0">
+							<li class="text-center mb-2 mr-2"><a href="javascript:void(0);" class="md-round d-block py-2 px-2">Trend</a></li>
+							<li class="text-center mb-2"><a href="javascript:void(0);" class="md-round d-block py-2 px-2">Decor</a></li>
+							<li class="text-center mb-2 mr-2"><a href="javascript:void(0);" class="md-round d-block py-2 px-2">Plant</a></li>
+							<li class="text-center mb-2"><a href="javascript:void(0);" class="md-round d-block py-2 px-2">Table tree</a></li>
+							<li class="text-center mb-2 mr-2"><a href="javascript:void(0);" class="md-round d-block py-2 px-2">Bedroom tree</a></li>
+							<li class="text-center mb-2"><a href="javascript:void(0);" class="md-round d-block py-2 px-2">Living room</a></li>
+						</ul>
+					</div>
+				</div>
+			</aside>
+		</main>
+		<!-- footer -->
+		<footer id="footer" class="container-fluid overflow-hidden px-lg-20">
+			<div class="copyRightHolder text-center pt-lg-5 pb-lg-4 py-3">
+				<p class="mb-0">Coppyright 2019 by <a href="javascript:void(0);">Botanical Store</a> - All right reserved</p>
+			</div>
+		</footer>
+	</div>
+	<!-- include jQuery library -->
+	<script src="project/template/js/jquery-3.4.1.min.js"></script>
+	<!-- include bootstrap popper JavaScript -->
+	<script src="project/template/js/popper.min.js"></script>
+	<!-- include bootstrap JavaScript -->
+	<script src="project/template/js/bootstrap.min.js"></script>
+	<!-- include custom JavaScript -->
+	<script src="project/template/js/jqueryCustome.js"></script>
+</body>
+
+
+</html>
